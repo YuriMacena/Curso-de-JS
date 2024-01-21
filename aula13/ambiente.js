e1343f3e069fee671db03141dc9cf46ea5f72851
Yuri Macena <yurimacena10@gmail.com>
2024-01-21 12:10:33 -0300

    Create aula13

---
+++ aula13/ambiente.js
@@ -0,0 +1,26 @@
+a=1
+do {
+    console.log(`Passo ${a}`)
+    a++
+} while (a<=10) //rep com teste no final
+
+c=1
+while(c<=6){ //enquanto c for menor ou igual a 6, irá se repetir o que está dentro do bloco
+    console.log('Tudo bem?')
+    c++ //c irá receber +1 repetição até chegar a 6
+} //repetição mais usada
+
+b=1
+while(b<=5){
+    console.log(`Passo ${b}`)
+    b++
+}
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/ //pior para repetição
