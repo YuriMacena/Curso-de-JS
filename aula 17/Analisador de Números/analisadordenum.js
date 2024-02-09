37d74c9a909ddc80c83f685ef91082ff9b38da75
Yuri Macena <yurimacena10@gmail.com>
2024-02-09 09:16:39 -0300

    Exercício da aula 17 solucionado

--- aula 17/Analisador de Números/analisadordenum.js
+++ aula 17/Analisador de Números/analisadordenum.js
@@ -1,6 +1,6 @@
 let num = document.getElementById('txtnum')
 let list = document.querySelector('select#txtlist')
-let res = document.querySelector('div#res')
+let res = document.querySelector('div#result')
 let valores = [ ]
 
 function isNumber(n){
@@ -20,8 +20,40 @@ function inList(n, l){
 }
 function analisar(){
     if (isNumber(num.value) && !inList(num.value, valores)) {
-        window.alert('Ok.')
-    } else {
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `O valor ${num.value} adicionado.`
+        list.appendChild(item)
+        res.innerHTML = ''
+        } else {
         window.alert('Valor inválido ou já encontrado na lista.')
+    }
+    num.value = '' //apaga onde o número foi digitado
+    num.focus() //focus continua na opção de digitar
+}
+
+function final(){
+    if (valores.length == 0){
+        window.alert('Adicione valores antes de finalizar.')
+    } else {
+        let total = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+        for (let pos in valores) {
+            soma += valores[pos]
+            if (valores[pos] > maior)
+                maior = valores[pos]
+            if (valores[pos] < menor)
+                menor = valores[pos]
+        }
+        media = soma/total
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo, temos ${total} números cadastrados.</p>`
+        res.innerHTML += `<p>O maior valor informado foi ${maior}.</p>`
+        res.innerHTML += `<p>O menor valor informado foi ${menor}.</p>`
+        res.innerHTML += `<p>Somando todos os valores, temos ${soma}.</p>`
+        res.innerHTML += `<p>A média dos valores digitados é ${media}.</p>`
     }
 }
